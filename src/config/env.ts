33a6b410b06c2d3b43eb66af7912995cad05f851
eduardo fuentes <eduardo.fuentesreyes@gmail.com>
2021-05-20 21:31:09 -0400

    Heroku

--- src/config/env.ts
+++ src/config/env.ts
@@ -1,7 +1,7 @@
 import dotenv from 'dotenv';
 
 process.env.NODE_ENV = process.env.NODE_ENV || 'development';
-const envFound = dotenv.config();
+//const envFound = dotenv.config();
 
 //if(envFound.error) throw new Error("Couldn't find .env file");
 
