68e1eaa5eec4160bdf93bf3484783e7cd9f1870e
eduardo fuentes <eduardo.fuentesreyes@gmail.com>
2021-05-22 22:21:01 -0400

    Heroku

--- src/test/routes.test.ts
+++ src/test/routes.test.ts
@@ -1,14 +1,14 @@
 import request from "supertest";
-import { app, db } from '../index';
+import { app, db } from "../index";
 
-
+beforeAll((done) => done());
+afterAll(() => {
+  db.close();
+});
 describe("Products Routes", () => {
-  afterAll(() => {
-    db.close();
-  });
-  it("Show all Products", async done => {
-     const res = await request(app.get).get("/api/product/findAll");
-     expect(res.status).toBe(200);
-     done();
+  it("Show all Products", async (done) => {
+    const res = await request(app.get).get("/api/product/findAll");
+    expect(res.status).toBe(200);
+    done();
   });
 });
